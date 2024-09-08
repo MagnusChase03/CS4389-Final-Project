4084a36427f77113c2d259d2216049db5eaed554
Zachary <zachness2003@gmail.com>
2024-09-07 19:30:24 -0500

    Build script comments

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
+# File name: build.sh
+# Description: Used to build all required containers.
+# Author: MagnusChase03
+
 #!/bin/bash
 
+# Build a specified container.
+# 
+# Arguments:
+#     - $1: The name of the folder which contains a Dockerfile to be built.
+#
+# Returns:
+#     - N/A
 function buildContainer() {
     cd $1
     sudo podman build -t localhost/cs4389-$1 .
     cd ../
 }
 
+# Build containers
 buildContainer api
