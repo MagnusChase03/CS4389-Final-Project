3d210a6ba1f71eb2b62c2b295a7da33069ac5ed0
Zachary <zachness2003@gmail.com>
2024-09-09 09:49:57 -0500

    Fixed path

--- api/main.go
+++ api/main.go
@@ -26,7 +26,7 @@ func main() {
 
     // Start HTTPS server on port 8080
     fmt.Printf("[LOG] Starting API server on 0.0.0.0:8080.\n");
-    if err := http.ListenAndServeTLS("0.0.0.0:8080", "../certs/server.crt", "../certs/server.key", mux); err != nil {
+    if err := http.ListenAndServeTLS("0.0.0.0:8080", "/certs/server.crt", "/certs/server.key", mux); err != nil {
         fmt.Fprintf(os.Stderr, "[ERROR] Failed to start API server. %v\n", err);
     }
 }
