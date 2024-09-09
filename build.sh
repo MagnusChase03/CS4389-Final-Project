8ea691ad03fdc5d85b09bc361fe8c2d3b36c3197
Zachary <zachness2003@gmail.com>
2024-09-09 09:59:55 -0500

    Init redis

--- build.sh
+++ build.sh
@@ -7,14 +7,16 @@
 # Build a specified container.
 # 
 # Arguments:
-#     - $1: The name of the folder which contains a Dockerfile to be built.
+#     - $1: The name of the container
+#     - $2: The name of the folder which contains a Dockerfile to be built.
 #
 # Returns:
 #     - N/A
 function buildContainer() {
-    sudo podman build -t localhost/cs4389-$1 -f $1/Dockerfile .
+    sudo podman build -t localhost/cs4389-$1 -f $2/Dockerfile .
 }
 
 # Build containers
-buildContainer api
-buildContainer webapp
+buildContainer api ./api
+buildContainer webapp ./webapp
+buildContainer redis ./databases/redis
---
+++ databases/README.md
@@ -0,0 +1,4 @@
+# DB
+
+*The databases are responsible for keeping persistant data, caching data, and
+pub/sub for real time chat.*
